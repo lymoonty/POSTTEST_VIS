3bde9b75f0d64dc614b152044b0f2275c9c1843a
Nabila Putri Karni <bila7634@gmail.com>
2026-03-01 04:49:39 +0800

    Add project files.

---
+++ nyobaPT1/Form1.Designer.vb
@@ -0,0 +1,126 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        txtIP = New TextBox()
+        btnTambah = New Button()
+        btnReset = New Button()
+        txtIPK = New TextBox()
+        lblPredikat = New Label()
+        Label1 = New Label()
+        Label2 = New Label()
+        SuspendLayout()
+        ' 
+        ' txtIP
+        ' 
+        txtIP.BackColor = SystemColors.InactiveCaption
+        txtIP.ForeColor = SystemColors.InfoText
+        txtIP.Location = New Point(261, 141)
+        txtIP.Name = "txtIP"
+        txtIP.Size = New Size(144, 27)
+        txtIP.TabIndex = 0
+        ' 
+        ' btnTambah
+        ' 
+        btnTambah.BackColor = SystemColors.Control
+        btnTambah.Location = New Point(261, 191)
+        btnTambah.Name = "btnTambah"
+        btnTambah.Size = New Size(70, 29)
+        btnTambah.TabIndex = 1
+        btnTambah.Text = "Tambah"
+        btnTambah.UseVisualStyleBackColor = False
+        ' 
+        ' btnReset
+        ' 
+        btnReset.BackColor = SystemColors.Control
+        btnReset.Location = New Point(337, 191)
+        btnReset.Name = "btnReset"
+        btnReset.Size = New Size(68, 29)
+        btnReset.TabIndex = 2
+        btnReset.Text = "Reset"
+        btnReset.UseVisualStyleBackColor = False
+        ' 
+        ' txtIPK
+        ' 
+        txtIPK.BackColor = SystemColors.InactiveCaption
+        txtIPK.Location = New Point(261, 234)
+        txtIPK.Name = "txtIPK"
+        txtIPK.Size = New Size(144, 27)
+        txtIPK.TabIndex = 3
+        ' 
+        ' lblPredikat
+        ' 
+        lblPredikat.AutoSize = True
+        lblPredikat.Location = New Point(261, 277)
+        lblPredikat.Name = "lblPredikat"
+        lblPredikat.Size = New Size(15, 20)
+        lblPredikat.TabIndex = 4
+        lblPredikat.Text = "-"
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(137, 144)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(86, 20)
+        Label1.TabIndex = 5
+        Label1.Text = "IP Semester"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(137, 237)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(89, 20)
+        Label2.TabIndex = 6
+        Label2.Text = "IP Kumulatif"
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = SystemColors.Control
+        ClientSize = New Size(800, 450)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Controls.Add(lblPredikat)
+        Controls.Add(txtIPK)
+        Controls.Add(btnReset)
+        Controls.Add(btnTambah)
+        Controls.Add(txtIP)
+        ForeColor = SystemColors.ActiveCaptionText
+        Name = "Form1"
+        Text = "Kalkulator IP"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents txtIP As TextBox
+    Friend WithEvents btnTambah As Button
+    Friend WithEvents btnReset As Button
+    Friend WithEvents txtIPK As TextBox
+    Friend WithEvents lblPredikat As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+
+End Class
---
+++ nyobaPT1/Form1.vb
@@ -0,0 +1,48 @@
+﻿Public Class Form1
+    ' Mendeklarasi variabel di luar sub agar aplikasinya tetap berjalan
+    Dim totalIP As Double = 0
+    Dim jumlahSemester As Integer = 0
+
+    ' Tombol tambah untuk menghitung IPK dan menentukan predikat
+    Private Sub btnTambah_Click(sender As Object, e As EventArgs) Handles btnTambah.Click
+        Try
+            Dim ipInput As Double = Val(txtIP.Text)
+            If ipInput >= 0 And ipInput <= 4.0 Then
+                totalIP += ipInput
+                jumlahSemester += 1
+
+                ' Menghitung rata -rata IPk
+                Dim ipk As Double = totalIP / jumlahSemester
+                txtIPK.Text = Format(ipk, "0.00")
+
+                ' Menentukan predikat berdasarkan nilai IPk
+                If ipk >= 3.01 Then
+                    lblPredikat.Text = "Sangat Memuaskan"
+                ElseIf ipk >= 2.76 Then
+                    lblPredikat.Text = "Memuaskan"
+                ElseIf ipk >= 2.0 Then
+                    lblPredikat.Text = "Cukup"
+                Else
+                    lblPredikat.Text = "Kurang"
+                End If
+
+                ' Mengosongkan input IP untuk semester berikutnya
+                txtIP.Clear()
+                txtIP.Focus()
+            Else
+                MsgBox("Harap masukkan nilai IP antara 0 sampai 4!")
+            End If
+        Catch ex As Exception
+            MsgBox("Terjadi kesalahan!")
+        End Try
+    End Sub
+
+    ' Tombol reset untuk mengembalikan semuanya ke awal
+    Private Sub btnReset_Click(sender As Object, e As EventArgs) Handles btnReset.Click
+        totalIP = 0
+        jumlahSemester = 0
+        txtIP.Clear()
+        txtIPK.Clear()
+        lblPredikat.Text = "-"
+    End Sub
+End Class
